518d2557304b23058db698ece6b6eba54be96751
Matheus Streisky <streiskymatheus@gmail.com>
2026-05-01 18:17:57 -0300

    Criação do projeto

---
+++ src/main/java/com/streisky/precoreal/PrecorealApplication.java
@@ -0,0 +1,13 @@
+package com.streisky.precoreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecorealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecorealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streisky/precoreal/PrecorealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streisky.precoreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecorealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
